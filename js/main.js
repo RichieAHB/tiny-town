5ce692c076b2ef29f8303146651c96e28355f7d7
Richard Beddington <rich@suttonyoung.com>
2014-12-12 14:56:03 +0000

    Changed the aesthetics for a bit of eye candy while developing and added a few bits like stars, buildings etc.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,73 @@
-/*
- * Plugin Setup
- */
+function generateStars(count) {
+	while (count--) {
+		var size = Math.random() * 10;
+		$('<div class="star">').appendTo($('.stars')).css({
+			left: Math.random() * 100 + '%',
+			opacity: Math.random(),
+			height: size + 'px',
+			top: Math.random() * 100 + '%',
+			width: size + 'px',
+		});
+	}
+	
+}
+
+generateStars(100);
 
 $('#tt').tinyTown({
-	syncedAnimations: [{
-		selector: '.celestial',
-		animations: [{
-			type: 'rotate',
-			end: -180
-		}]
-	},{
-		selector: '.tt__static',
-		animations: [{
-			type: 'background-color',
-			start: '#ddeeff',
-			end: '#112233'
-		}]
-	},{
-		selector: '.car',
-		animations: [{
-			type: 'right',
-			end: 'out'
-		}]
-	},{
-		selector: '.balloon',
-		animations: [{
-			type: 'up',
-			end: 'out',
-			startPercentage: 0.5,
-			endPercentage: 0.6
-		}]
-	}]
+	syncedElements: [
+		{
+			selector: '.celestial',
+			animations: [{
+				type: 'rotate',
+				end: -180
+			}]
+		},{
+			selector: '.tt__static',
+			animations: [{
+				type: 'background-color',
+				start: '#ddeeff',
+				end: '#112233'
+			}]
+		},{
+			selector: '.moon',
+			animations: [{
+				type: 'background-color',
+				start: '#ddeeff',
+				end: '#112233'
+			}]
+		},{
+			selector: '.car',
+			animations: [{
+				type: 'right',
+				end: 'out'
+			}]
+		},{
+			selector: '.balloon',
+			animations: [{
+				type: 'up',
+				end: 'out',
+				startPercentage: 0.5,
+				endPercentage: 0.6
+			}]
+		},{
+			selector: '.stars',
+			animations: [{
+				type: 'opacity',
+				start: 0,
+				end: 1,
+				startPercentage: 0.6,
+				endPercentage: 1
+			}]
+		}
+	],
+	syncedTriggers: [
+		{
+			percentage: 0.5,
+			trigger: function() {
+				$('.car').addClass('rotate');
+			}
+		}
+	],
+	navItemSelector: '.navigation a'
 });
